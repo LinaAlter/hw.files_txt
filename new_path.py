10eaada69100e61ddac8449513ca648ae1fa6357
Anna Volkova <anotherwaygirl@yandex.ru>
2024-07-23 14:39:05 +0700

    try in new_path, txt removed to file_with_txt

--- new_path.py
+++ new_path.py
@@ -1,6 +1,39 @@
-with open('text_1.txt', encoding = 'utf-8') as file_1, open ('text_2.txt', encoding = 'utf-8') as file_2, open ('text_3.txt', encoding = 'utf-8') as file_3:
+import os
+
+folder_path = 'C:\\Users\\alcen\\OneDrive\\Рабочий стол\\hw.files_txt\\file_with_txt'
+def parse_folder(folder_path):
+
+    for root, dirs, files in os.walk(folder_path, topdown=False): 
+        files_names = []     
+        for name in files:
+            files_names.append(name)
+        print(files_names)
+        #return files_names
     
-        
+    for file in os.listdir(folder_path):
+        with open(file) as f:
+            files = []
+            files.append(file.readlines())
+            print(files)
+            len_f = []
+            
+            f.readlines()
+            len_f.append(len(f))
+        s_len_f = sorted(len_f)
+        print(s_len_f)
+            
+    
+parse_folder(folder_path)    
+ 
+
+
+
+
+
+
+
+    
+'''   
     files = (file_1.readlines()), file_2.readlines(), file_3.readlines()
     len_f = []
     for f in files:
@@ -12,14 +45,6 @@ with open('text_1.txt', encoding = 'utf-8') as file_1, open ('text_2.txt', encod
     files_for_dict = (file_1.read().rstrip(), file_2.read().rstrip(), file_3.read().rstrip())
     dict_f = dict(zip(len_f, files_for_dict))
     sort_dict_f = dict(sorted(dict_f.items())) 
-    
-# взять все файлы из конкретной папки, прочитать их поочередно(вероятно нужно прописать путь к папке)
-# для каждого измеряя количество строк
-    # тут должна быть функция, которая перепишет тксты в словарь и ключами длинами.
-    #  а тут функция, которая запишет в новый файл все сразу в нужном порядке
-
-    # КАК ВЫТАЩИТЬ НАЗВАНИЕ ФАЙЛА???
-      
 
    
 
@@ -36,11 +61,9 @@ with open('comb_file.txt', 'a', encoding = 'utf-8') as c_f:
     c_f.writelines(str(list(sort_dict_f.values())[2]))
 
         
-       
+'''       
     
 
   
 
   
-   
-          
